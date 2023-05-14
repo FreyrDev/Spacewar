a717a549cad35d10ffe4a17b59ba5ca6383b2255
Freyr <95385132+FreyrDev@users.noreply.github.com>
2023-05-14 06:18:33 +0100

    Basic input controls

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@
 #endif
 
 enum Type {
-  PLAYER,
+  PLAYER1,
+  PLAYER2,
   BULLET,
   STAR
 };
@@ -25,6 +26,7 @@ void setup() {
   setlocale(LC_ALL, "");
   initscr();
   noecho();
+  nodelay(stdscr, TRUE);
   cbreak();
   keypad(stdscr, TRUE);
   start_color();
@@ -45,12 +47,6 @@ void setup() {
 void wcolour(WINDOW *win,int col) { wattron(win, COLOR_PAIR(col+1)); }
 void colour(int col) { wcolour(stdscr, col); }
 
-GameObject create_gravity_source(WINDOW *win, int y, int x, int mass, char ch) {
-  mvwaddch(win, y, x, ch);
-  GameObject gravity = {STAR, y, x, 0, 0, 0, mass};
-  return gravity;
-}
-
 int get_delta(struct timespec *start, struct timespec *end) {
   int result = 0;
   result += end->tv_sec - start->tv_sec;
@@ -59,8 +55,11 @@ int get_delta(struct timespec *start, struct timespec *end) {
 }
 
 char charof(enum Type type) {
-  if (type == PLAYER) {
+  if (type == PLAYER1) {
     return '1';
+  }
+  if (type == PLAYER2) {
+    return '2';
   }
   else if (type == BULLET) {
     return '*';
@@ -70,11 +69,19 @@ char charof(enum Type type) {
   }
 }
 
-void update_physics(GameObject *objects, int num) {
+void update_physics(WINDOW* game_win, GameObject *objects, int num) {
+  int winh;
+  int winw;
+  getmaxyx(game_win, winh, winw);
+
   int i = 0;
   while (i < num) {
-    (objects+i)->y += (objects+i)->vely;
-    (objects+i)->x += (objects+i)->velx;
+    objects[i].y += objects[i].vely;
+    objects[i].x += objects[i].velx;
+    if (objects[i].y >= winh-1) { objects[i].y -= winh-2; }
+    else if (objects[i].y <= 1) { objects[i].y += winh-2; }
+    if (objects[i].x >= winw-1) { objects[i].x -= winw-2; }
+    else if (objects[i].x <= 1) { objects[i].x += winw-2; }
     i++;
   }
 }
@@ -102,9 +109,10 @@ int main() {
   WINDOW *game = newwin(gameh, gamew, (scrh-gameh)/2, (scrw-gamew)/2);
   wcolour(game, 0);
 
-  GameObject black_hole = create_gravity_source(game, gameh/2,gamew/2,100,'@');
-  GameObject p1 = {PLAYER, 10, 10, 0.5, 1, 0, 1};
-  GameObject game_objects[64] = {black_hole, p1};
+  GameObject black_hole = {STAR, gameh/2, gamew/2, 0, 0, 0, 100};
+  GameObject p1 = {PLAYER1, 38, 20, 0, 0, 0, 1};
+  GameObject p2 = {PLAYER2, 12, 80, 0, 0, 180, 1};
+  GameObject game_objects[64] = {black_hole, p1, p2};
 
   struct timespec start;
   clock_gettime(CLOCK_MONOTONIC_RAW, &start);
@@ -113,13 +121,35 @@ int main() {
     if (delta >= 33333333) {
       clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-      update_physics(game_objects, 2);
-      update_screen(game, game_objects, 2);
-      wnoutrefresh(game);
+      int keys_pressed[8] = {-1,-1,-1,-1,-1,-1,-1,-1};
+      int ch;
+      int ch_num = 0;
+      while ((ch = getch()) != ERR) {
+        keys_pressed[ch_num] = ch;
+        ch_num++;
+      }
+      keys_pressed[ch_num] = ERR;
 
-      mvprintw(0,0, "%d", 1000000000/delta);
-      wnoutrefresh(stdscr);
-      doupdate();
+      move(0,0);
+      clrtoeol();
+      for (int i = 0; i < 8 && keys_pressed[i] != ERR; i++) {
+        if(keys_pressed[i] == 'w') {
+          game_objects[1].vely -= 0.005;
+        }
+        if(keys_pressed[i] == 'a') {
+          game_objects[1].velx -= 0.005;
+        }
+        if(keys_pressed[i] == 's') {
+          game_objects[1].vely += 0.005;
+        }
+        if(keys_pressed[i] == 'd') {
+          game_objects[1].velx += 0.005;
+        }
+      }
+
+      update_physics(game, game_objects, 3);
+      update_screen(game, game_objects, 3);
+      wrefresh(game);
 
       delta = 0;
     }
